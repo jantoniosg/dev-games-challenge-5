eccd72c8643e1652a62f05e156d3611c3dffea1b
Jesús Antonio Sarco Giannini <jesus.antonio.sarco@gmail.com>
2021-10-06 11:33:41 +0200

    commit changes

--- src/main/java/com/santander/games/challenges/quarkus/GreetingResource.java
+++ src/main/java/com/santander/games/challenges/quarkus/GreetingResource.java
@@ -1,5 +1,11 @@
 package com.santander.games.challenges.quarkus;
 
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeIn;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
+import org.eclipse.microprofile.openapi.annotations.security.SecuritySchemes;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -7,9 +13,15 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("")
+@SecuritySchemes(value = {
+        @SecurityScheme(type = SecuritySchemeType.APIKEY,
+                description = "api-key for 3scale",
+                securitySchemeName = "api-key",
+                in = SecuritySchemeIn.HEADER)})
 public class GreetingResource {
 
   @GET
+  @SecurityRequirement(name = "api-key")
   @Path("/hello")
   @Produces(MediaType.TEXT_PLAIN)
   public String hello() {
@@ -17,6 +29,7 @@ public class GreetingResource {
   }
 
   @GET
+  @SecurityRequirement(name = "api-key")
   @Path("/hello/{name}")
   @Produces(MediaType.TEXT_PLAIN)
   public String helloName(@PathParam("name") String name) {
@@ -24,6 +37,7 @@ public class GreetingResource {
   }
 
   @GET
+  @SecurityRequirement(name = "api-key")
   @Path("/goodbye")
   @Produces(MediaType.TEXT_PLAIN)
   public String goodbye() {
@@ -31,6 +45,7 @@ public class GreetingResource {
   }
 
   @GET
+  @SecurityRequirement(name = "api-key")
   @Path("/goodbye/{name}")
   @Produces(MediaType.TEXT_PLAIN)
   public String goodbyeName(@PathParam("name") String name) {
